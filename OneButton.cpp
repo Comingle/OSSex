03cae3a92edc08dd5886d596e7ba47c3292a12db
Craig <craigdurkin@gmail.com>
2014-08-17 22:20:20 -0400

    add onebutton library

---
+++ OneButton.cpp
@@ -0,0 +1,237 @@
+// -----
+// OneButton.cpp - Library for detecting button clicks, doubleclicks and long press pattern on a single button.
+// This class is implemented for use with the Arduino environment.
+// Copyright (c) by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// More information on: http://www.mathertel.de/Arduino
+// -----
+// Connect the button to pin A1 (MenuPin)
+// 02.10.2010 created by Matthias Hertel
+// 21.04.2011 support of active LOW and active HIGH button signal input.
+// 01.12.2011 include file changed to work with the Arduino 1.0 environment
+// 12.01.2014 some typos fixed.
+// 01.03.2014 Enhanced long press functionalities by adding longPressStart and longPressStop callbacks
+// -----
+
+#include "OneButton.h"
+
+// ----- Initialization and Default Values -----
+
+OneButton::OneButton(int pin, int activeLow)
+{
+  pinMode(pin, INPUT);      // sets the MenuPin as input
+  _pin = pin;
+
+  _clickTicks = 450;        // number of millisec that have to pass by before a click is detected.
+  _pressTicks = 1000;       // number of millisec that have to pass by before a long button press is detected.
+ 
+  _state = 0; // starting with state 0: waiting for button to be pressed
+  _isLongPressed = false;  // Keep track of long press state
+
+  if (activeLow) {
+    // button connects ground to the pin when pressed.
+    _buttonReleased = HIGH; // notPressed
+    _buttonPressed = LOW;
+    pinMode(pin, INPUT_PULLUP);   // turn on pullUp resistor
+
+  } else {
+    // button connects VCC to the pin when pressed.
+    _buttonReleased = LOW;
+    _buttonPressed = HIGH;
+  } // if
+  // Debounce init
+  _db_buttonState      = _buttonReleased;  // the current reading from the input pin, assume its in rest during setup
+  _db_lastButtonState  = _db_buttonState;  // the previous reading from the input pin, no previous reading so use current
+  _db_lastDebounceTime = 0;                // the last time the output pin was toggled, init to zero
+  _db_debounceDelay    = 25;               // the debounce time; increase if the output flickers. Settable with setDebounceDelay() function
+
+} // OneButton
+
+OneButton::OneButton() {
+
+  _clickTicks = 450;        // number of millisec that have to pass by before a click is detected.
+  _pressTicks = 1000;       // number of millisec that have to pass by before a long button press is detected.
+
+  _state = 0; // starting with state 0: waiting for button to be pressed
+  _isLongPressed = false;  // Keep track of long press state
+
+  // Debounce init
+  _db_buttonState      = _buttonReleased;  // the current reading from the input pin, assume its in rest during setup
+  _db_lastButtonState  = _db_buttonState;  // the previous reading from the input pin, no previous reading so use current
+  _db_lastDebounceTime = 0;                // the last time the output pin was toggled, init to zero
+  _db_debounceDelay    = 25;               // the debounce time; increase if the output flickers. Settable with setDebounceDelay() function
+}
+
+void OneButton::setPin(int pin) {
+  pinMode(pin, INPUT);      // sets the MenuPin as input
+  _pin = pin;
+}
+
+void OneButton::setActiveLow(int activeLow) {
+  if (activeLow) {
+    // button connects ground to the pin when pressed.
+    _buttonReleased = HIGH; // notPressed
+    _buttonPressed = LOW;
+    pinMode(_pin, INPUT_PULLUP);   // turn on pullUp resistor
+
+  } else {
+    // button connects VCC to the pin when pressed.
+    _buttonReleased = LOW;
+    _buttonPressed = HIGH;
+  } // if
+}
+
+// explicitly set the number of millisec that have to pass by before a click is detected.
+void OneButton::setClickTicks(int ticks) { 
+  _clickTicks = ticks;
+} // setClickTicks
+
+
+// explicitly set the number of millisec that have to pass by before a long button press is detected.
+void OneButton::setPressTicks(int ticks) {
+  _pressTicks = ticks;
+} // setPressTicks
+
+
+// save function for click event
+void OneButton::attachClick(callbackFunction newFunction)
+{
+  _clickFunc = newFunction;
+} // attachClick
+
+
+// save function for doubleClick event
+void OneButton::attachDoubleClick(callbackFunction newFunction)
+{
+  _doubleClickFunc = newFunction;
+} // attachDoubleClick
+
+
+// save function for press event
+// DEPRECATED, is replaced by attachLongPressStart, attachLongPressStop, attachDuringLongPress, 
+void OneButton::attachPress(callbackFunction newFunction)
+{
+  _pressFunc = newFunction;
+} // attachPress
+
+// save function for longPressStart event
+void OneButton::attachLongPressStart(callbackFunction newFunction)
+{
+  _longPressStartFunc = newFunction;
+} // attachLongPressStart
+
+// save function for longPressStop event
+void OneButton::attachLongPressStop(callbackFunction newFunction)
+{
+  _longPressStopFunc = newFunction;
+} // attachLongPressStop
+
+// save function for during longPress event
+void OneButton::attachDuringLongPress(callbackFunction newFunction)
+{
+  _duringLongPressFunc = newFunction;
+} // attachDuringLongPress
+
+// function to get the current long pressed state
+bool OneButton::isLongPressed(){
+  return _isLongPressed;
+}
+
+int OneButton::tick(void)
+{
+  // Detect the input information 
+  int buttonLevel = debounce(digitalRead(_pin)); // current button signal. (debounced)
+  unsigned long now = millis(); // current (relative) time in msecs.
+
+  // Implementation of the state machine
+  if (_state == 0) { // waiting for menu pin being pressed.
+    if (buttonLevel == _buttonPressed) {
+      _state = 1; // step to state 1
+      _startTime = now; // remember starting time
+    } // if
+
+  } else if (_state == 1) { // waiting for menu pin being released.
+    if (buttonLevel == _buttonReleased) {
+      _state = 2; // step to state 2
+
+    } else if ((buttonLevel == _buttonPressed) && (now > _startTime + _pressTicks)) {
+      _isLongPressed = true;  // Keep track of long press state
+      if (_pressFunc) _pressFunc();
+	  if (_longPressStartFunc) _longPressStartFunc();
+	  if (_duringLongPressFunc) _duringLongPressFunc();
+      _state = 6; // step to state 6
+      
+    } else {
+      // wait. Stay in this state.
+    } // if
+
+  } else if (_state == 2) { // waiting for menu pin being pressed the second time or timeout.
+    if (now > _startTime + _clickTicks) {
+      // this was only a single short click
+      if (_clickFunc) _clickFunc();
+      _state = 0; // restart.
+
+    } else if (buttonLevel == _buttonPressed) {
+      _state = 3; // step to state 3
+    } // if
+
+  } else if (_state == 3) { // waiting for menu pin being released finally.
+    if (buttonLevel == _buttonReleased) {
+      // this was a 2 click sequence.
+      if (_doubleClickFunc) _doubleClickFunc();
+      _state = 0; // restart.
+    } // if
+
+  } else if (_state == 6) { // waiting for menu pin being release after long press.
+    if (buttonLevel == _buttonReleased) {
+	  _isLongPressed = false;  // Keep track of long press state
+	  if(_longPressStopFunc) _longPressStopFunc();
+      _state = 0; // restart.
+    } else {
+	  // button is being long pressed
+	  _isLongPressed = true; // Keep track of long press state
+	  if (_duringLongPressFunc) _duringLongPressFunc();
+    } // if  
+
+  } // if  
+  return _state;
+} // OneButton.tick()
+
+boolean OneButton::debounce(boolean reading){
+  // Source: http://arduino.cc/en/Tutorial/Debounce
+  
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != _db_lastButtonState) {
+    // reset the debouncing timer
+    _db_lastDebounceTime = millis();
+  }
+ 
+  if ((millis() - _db_lastDebounceTime) > _db_debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != _db_buttonState) {
+      _db_buttonState = reading;
+    }
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  _db_lastButtonState = reading;
+
+  return _db_buttonState;
+}
+
+void OneButton::setDebounceDelay(int delay){
+	_db_debounceDelay = delay;
+}
+
+
+
+// end.
+
---
+++ OneButton.h
@@ -0,0 +1,98 @@
+// -----
+// OneButton.h - Library for detecting button clicks, doubleclicks and long press pattern on a single button.
+// This class is implemented for use with the Arduino environment.
+// Copyright (c) by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// More information on: http://www.mathertel.de/Arduino
+// -----
+// 02.10.2010 created by Matthias Hertel
+// 21.04.2011 transformed into a library
+// 01.12.2011 include file changed to work with the Arduino 1.0 environment
+// 23.03.2014 Enhanced long press functionalities by adding longPressStart and longPressStop callbacks
+// -----
+
+#ifndef OneButton_h
+#define OneButton_h
+
+#include "Arduino.h"
+
+// ----- Callback function types -----
+
+extern "C" {
+  typedef void (*callbackFunction)(void);
+}
+
+
+class OneButton
+{
+public:
+  // ----- Constructor -----
+  OneButton(int pin, int active);
+  OneButton();
+  
+  // ----- Set runtime parameters -----
+
+  // set button pin
+  void setPin(int pin);
+
+  // set if button is active when low
+  void setActiveLow(int activeLow);
+
+  // set # millisec after single click is assumed.
+  void setClickTicks(int ticks);
+
+  // set # millisec after press is assumed.
+  void setPressTicks(int ticks);
+
+  // attach functions that will be called when button was pressed in the specified way.
+  void attachClick(callbackFunction newFunction);
+  void attachDoubleClick(callbackFunction newFunction);
+  void attachPress(callbackFunction newFunction); // DEPRECATED, replaced by longPressStart, longPressStop and duringLongPress
+  void attachLongPressStart(callbackFunction newFunction);
+  void attachLongPressStop(callbackFunction newFunction);
+  void attachDuringLongPress(callbackFunction newFunction);
+
+  // ----- State machine functions -----
+
+  // call this function every some milliseconds for handling button events.
+  int tick(void);
+  bool isLongPressed();
+
+  // ----- public debounce functions ------
+  void setDebounceDelay(int delay);
+
+private:
+  int _pin;        // hardware pin number. 
+  int _clickTicks; // number of ticks that have to pass by before a click is detected
+  int _pressTicks; // number of ticks that have to pass by before a lonn button press is detected
+
+  int _buttonReleased;
+  int _buttonPressed;
+
+  bool _isLongPressed;
+
+  // These variables will hold functions acting as event source.
+  callbackFunction _clickFunc;
+  callbackFunction _doubleClickFunc;
+  callbackFunction _pressFunc;
+  callbackFunction _longPressStartFunc;
+  callbackFunction _longPressStopFunc;
+  callbackFunction _duringLongPressFunc;
+
+  // These variables that hold information across the upcoming tick calls.
+  // They are initialized once on program start and are updated every time the tick function is called.
+  int _state;
+  unsigned long _startTime; // will be set in state 1
+
+  // Debounce variables and functions
+  // Help prevent bouncing button states from being read as double clicks
+  int _db_buttonState;                // the current reading from the input pin
+  int _db_lastButtonState;            // the previous reading from the input pin
+  unsigned long _db_lastDebounceTime; // the last time the output pin was toggled
+  unsigned long _db_debounceDelay;    // the debounce time; increase if the output flickers
+  boolean debounce(boolean reading);  // method to debounce the digital reading, call every loop
+};
+
+#endif
+
+
